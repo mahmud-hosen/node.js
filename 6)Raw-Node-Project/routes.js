b986cbdc9881ad331adf9781e99845230b447422
mahmud-hosen <mahmudhossain582@gmail.com>
2023-07-21 20:46:39 +0600

    part-3 done

--- 6)Raw-Node-Project/routes.js
+++ 6)Raw-Node-Project/routes.js
@@ -6,8 +6,11 @@ Date:08-04-2023
 
 // Dependencies
 const {sampleHandler} = require('./handlers/routeHandlers/sampleHandler')
+const {userHandler} = require('./handlers/routeHandlers/userHandler')
+
 
 const routes = {
     sample: sampleHandler,
+    user: userHandler
 }
 module.exports = routes;
